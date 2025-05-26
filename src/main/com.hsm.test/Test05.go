4adaa3961715e17a83257ff15639d00036df3a4c
suminhe <suminhe@tencent.com>
2025-05-26 19:22:23 +0800

    两数相加

---
+++ src/main/com.hsm.test/Test05.go
@@ -0,0 +1,19 @@
+package main
+
+/* 函数返回两个数的最大值 */
+func max(num1, num2 int) int {
+	/* 声明局部变量 */
+	var result int
+
+	if num1 > num2 {
+		result = num1
+	} else {
+		result = num2
+	}
+	return result
+}
+
+func main() {
+	count := max(10, 11)
+	println("最大值是：", count)
+}
