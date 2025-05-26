6bce8486c54a297938f80ceb184f3dfb5cbcd034
suminhe <suminhe@tencent.com>
2025-05-26 19:43:36 +0800

    两数相加

--- src/main/com.hsm.test/Test05.go
+++ src/main/com.hsm.test/Test05.go
@@ -2,9 +2,21 @@ package main
 
 import "fmt"
 
+func main() {
+	/* 定义局部变量 */
+	var a int = 100
+	var b int = 200
+	var ret int
+
+	/* 调用函数并返回最大值 */
+	ret = max(a, b)
+
+	fmt.Printf("最大值是 : %d\n", ret)
+}
+
 /* 函数返回两个数的最大值 */
 func max(num1, num2 int) int {
-	/* 声明局部变量 */
+	/* 定义局部变量 */
 	var result int
 
 	if num1 > num2 {
@@ -13,9 +25,4 @@ func max(num1, num2 int) int {
 		result = num2
 	}
 	return result
-}
-
-func main() {
-	count := max(10, 11)
-	fmt.Println("最大值是：", count)
 }
