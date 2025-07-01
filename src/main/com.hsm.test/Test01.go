7db798010b87ebbd64398a9ae34655b61e2362a7
suminhe <suminhe@tencent.com>
2025-07-01 14:53:24 +0800

    新增 获取系统环境变量

--- src/main/com.hsm.test/Test01.go
+++ src/main/com.hsm.test/Test01.go
@@ -1,25 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-
-	var a, b int
-	var pwd int = 20221020
-
-	// 用户输入
-	fmt.Scan(&a)
-
-	if a == pwd {
-		fmt.Println("请再次输入密码：")
-		fmt.Scan(&b)
-		if b == pwd {
-			fmt.Println("登录成功")
-		} else {
-			fmt.Println("密码错误")
-		}
-
-	} else {
-		fmt.Println("登录失败")
-	}
+	fmt.Println("CYPHER_KEY:", os.Getenv("CYPHER_KEY"))
 }
